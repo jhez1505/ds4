6252e7de3b81669030660b7d88697f103070b898
JHEZRREL DELGADO <jhezrrel.delgado@utp.ac.pa>
2025-09-15 19:55:29 -0500

    Actualizacion de laboratorios

---
+++ Laboratorio 2/Laboratorio 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Laboratorio2;
+
+    class Program
+    {
+        static void Main(String[] args)
+        {
+        Client client = new Client();
+        client.Firstname = "Su_nombre";
+        client.Lastname = "Su apellido";
+        client.Age = 15;
+        client.ID = 1;
+
+        Console.WriteLine(client.GetFullName());
+        }
+
+
+
+public class Client
+{
+    public int ID { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public ushort Age { get; set; }
+
+    public string GetFullName()
+    { 
+        return Firstname + " " + Lastname;
+    }
+}
+
+}
---
+++ Laboratorio 3/Laboratorio 32/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class CalculosMatematicos
+    {
+        public double CalculoArea(double radio)
+        {
+            return Math.PI * Math.Pow(radio, 2);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculosMatematicos calculos = new CalculosMatematicos();
+
+            Console.WriteLine("=== EJERCICIO 2: Área de un círculo ===");
+            Console.Write("Ingrese el radio del círculo: ");
+            double radio = double.Parse(Console.ReadLine());
+
+            double area = calculos.CalculoArea(radio);
+            Console.WriteLine($"El área del círculo con radio {radio} es: {area}");
+
+            Console.ReadKey();
+        }
+    }
+}
