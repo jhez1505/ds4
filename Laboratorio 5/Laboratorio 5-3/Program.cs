36d01e1792c8779e1dc01ff2e29af825c1f5af53
JHEZRREL DELGADO <jhezrrel.delgado@utp.ac.pa>
2025-09-22 20:11:48 -0500

    Laboratorios 5-6 completados

---
+++ Laboratorio 5/Laboratorio 5-3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Programa
+{
+    static void Main(string[] args)
+    {
+        string[] frutas = { "manzana", "platano", "naranja" };
+
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
+    }
+}
